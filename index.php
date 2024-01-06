0e3c3c636e00fc4407febf4e551fb913ca457f67
Kimmy169 <83825284+Kimmy169@users.noreply.github.com>
2024-01-06 20:56:07 +0100

    update

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<link rel="icon" type="image/x-icon" href="D:\GitHub projects\Webov-st-rnka-kv-z\icons8-question-mark-16.png">
+<body>
+<h1 style="text-align:center;">Kvíz všeobecných znalostí</h1>
+<h2 style="text-align:center">Kolik hráčů bude hrát ?</h2>
+
+<button class="button button1">1</button>
+<button class="button button1">2</button>
+</body>
+</html>
+<?php
+
+
+
+?>
